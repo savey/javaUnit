1ffd7f60ad7bc4291fe8a65fa8eb5d14daa69cda
Savey <64387552@qq.com>
2020-11-09 11:17:58 +0800

    fixg

--- src/com/unitFile/Session4Test.java
+++ src/com/unitFile/Session4Test.java
@@ -5,7 +5,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
+import java.nio.file.CopyOption;
 import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class Session4Test {
     public static void main(String[] args) throws IOException {
@@ -17,7 +19,7 @@ public class Session4Test {
     private static void copyFile(String sour, String des) {
         long startTime = System.currentTimeMillis();
         try {
-            Files.copy(new File(sour).toPath(), new File(des).toPath());
+            Files.copy(new File(sour).toPath(), new File(des).toPath(), StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             e.printStackTrace();
         }
